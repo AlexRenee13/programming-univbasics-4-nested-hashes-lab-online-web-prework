cd30482e234acf1b0e057231d7977af93ac92226
Alexis Palacios <alexis1994662@yahoo.com>
2019-10-28 22:52:29 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -21,7 +21,7 @@ end
 
 def alan_kay_is_known_for
 	# What combination of keys would you use to return the value of the :known_for key of :alan_kay?
-	programmer_hash[:alan_kay][:known_for]
+# 	programmer_hash[:alan_kay][:known_for]
 	
 	programmer_hash = 
  		{
@@ -82,7 +82,7 @@ def adding_matz
         }
      }
 
-    programmer_hash[:yukihiro_matsumoto => {:known_for=> "Ruby", :languages => ["LISP","C"]}
+    # programmer_hash[:yukihiro_matsumoto => {:known_for=> "Ruby", :languages => ["LISP","C"]}
 end
 
 def changing_alan
@@ -103,7 +103,7 @@ def changing_alan
      }
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
      alans_new_info = "GUI"
-     programmer_hash[:alan_kay][:known_for]="GUI"
+    # programmer_hash[:alan_kay][:known_for]="GUI"
      
 end
 
