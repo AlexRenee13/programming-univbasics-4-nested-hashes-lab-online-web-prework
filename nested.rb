134b9b6e69d4349b05c0dcc5dc36ecffa2ecaa12
Alexis Palacios <alexis1994662@yahoo.com>
2019-10-28 22:50:06 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -64,7 +64,7 @@ def adding_matz
 #   :known_for => "Ruby",
 #    :languages => ["LISP", "C"]
 # }
-programmer_hash[:yukihiro_matsumoto]
+programmer_hash[:yukihiro_matsumoto => {:known_for=> "Ruby", :languages => ["LISP","C"]}
 
 	programmer_hash = 
  		{
@@ -103,7 +103,7 @@ def changing_alan
      }
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
      alans_new_info = "GUI"
-     
+     programmer_hash=
      
 end
 
