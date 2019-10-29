47bdabe53ddcff2250cb0c072df7b56a8c3ba735
Alexis Palacios <alexis1994662@yahoo.com>
2019-10-29 00:23:19 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -71,7 +71,7 @@ def adding_matz
 
 	programmer_hash = 
  		{
- 		  
+ 		  :yukihiro_matsumoto=> {:known_for => "Ruby", :languages => ["LISP", "C"]},
         :grace_hopper => {
           :known_for => "COBOL",
           :languages => ["COBOL", "FORTRAN"]
@@ -85,9 +85,6 @@ def adding_matz
           :languages => ["C"]
         }
  		}
-  programmer_hash[:yukihiro_matsumoto]=:known_for=> "Ruby", 
- 		    :languages => ["LISP","C"]
- 		    },
     
   return
 end
